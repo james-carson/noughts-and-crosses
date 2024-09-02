4c4edab0834b16cead558550658daf009f47e0cd
carsoninho <james.87.carson@gmail.com>
2024-09-02 19:38:59 +0800

    Setup of basic game - testing logic now

--- script.js
+++ script.js
@@ -79,22 +79,95 @@ const player2 = new Player(playersInfo.player2Name, playersInfo.player2Symbol);
 
 // Player setup is now working!
 
-// TODO - Now to start playing the game itself************
+let activePlayer = 1
 
-// Logic for resetting the board
-
-const resetBoard = function () {
-
+function switchPlayer() {
+    activePlayer === 1 ? activePlayer = 2 : activePlayer = 1
+    console.log(`Player switched to Player ${activePlayer}`)
 }
 
-// Logic for playing a turn
+const playGame = function (turnChoice) {
+    if (activePlayer === 1) {
+        turnChoice = parseInt(prompt(`Which square would you like your ${player1.symbol} in?`), 10);
+    } else {
+        turnChoice = parseInt(prompt(`Which square would you like your ${player2.symbol} in?`), 10);
+    }
 
-const playTurn = function () {
+    console.log(`The player chose square ${turnChoice}`)
 
+    if (turnChoice > 0 && turnChoice < 10) {
+        for (let i = 0; i < gameboard.length; i++) {
+            for (let j = 0; j < gameboard.length; j++) {
+                if (gameboard[i][j][0] === turnChoice) {
+                    if (gameboard[i][j][1] === '') {
+                        gameboard[i][j][1] = (activePlayer === 1 ? player1.symbol : player2.symbol);
+                        console.log(`cell updated to ${gameboard[i][j][1]}`)
+                    } else {
+                        alert("Oops. Please choose another square.");
+                        takeTurn();
+                    }
+                }
+            }
+        }
+    } else {
+        alert("Please enter a number between 1 and 9");
+        takeTurn();
+    }
+    
+    checkWin();
+    switchPlayer();
+    console.log(`The player is now ${activePlayer}`)
+    console.log(gameboard)
+    playGame();
+};
+
+// This seems to be working - need to check with an actual game!
+
+// Logic for resetting the board - This probably won't be needed
+
+function resetBoard() {
+    const columns = 3;
+    const rows = 3;
+    const board = [];
+    let cell = 1;
+    let value = '';
+
+    for (let i = 0; i < columns; i++) {
+        board[i] = [];
+        for (let j = 0; j < rows; j++) {
+            board[i].push([cell, value]);
+            cell++;
+        }
+    }
 }
 
 // Logic for checking for a win
 
 const checkWin = function () {
 
+    const winningConditions = [
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 5, 8],
+        [3, 5, 7]
+    ];
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        const [x, y, z] = winningConditions[i]
+
+        if (activePlayer === 1) {
+            if (gameboard[x] === player1Symbol && gameboard[y] === player1Symbol && gameboard[z] === player1Symbol) {
+                alert(`${player1} wins! The game will now reset`)
+                resetBoard();
+            } else if
+                (gameboard[x] === player2Symbol && gameboard[y] === player2Symbol && gameboard[z] === player2Symbol) {
+                alert(`${player2} wins! The game will now reset`)
+                resetBoard();
+            }
+        }
+    }
 }
